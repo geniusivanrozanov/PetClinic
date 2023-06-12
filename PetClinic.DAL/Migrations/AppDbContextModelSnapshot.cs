54c0b05699b4f907198230468a6c136c94324337
Veronika <veronikayatskova03@gmail.com>
2023-06-12 17:25:15 +0300

    Organize folders

--- PetClinic.DAL/Migrations/AppDbContextModelSnapshot.cs
+++ PetClinic.DAL/Migrations/AppDbContextModelSnapshot.cs
@@ -198,28 +198,28 @@ namespace PetClinic.DAL.Migrations
                         {
                             Id = new Guid("ddc19540-04df-4697-8237-3c74ff4e38cd"),
                             Address = "пр. Независимости, 177",
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Вет-клиника филиал 1",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         },
                         new
                         {
                             Id = new Guid("328b1872-1141-47f5-8f67-62c50562ad39"),
                             Address = "ул. Академическая, 26",
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Вет-клиника филиал 2",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         },
                         new
                         {
                             Id = new Guid("de1e6cc5-3e62-4459-9496-8a5fc0b2593f"),
                             Address = "ул. Карастояновой, 2",
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Вет-клиника филиал 3",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         });
                 });
 
@@ -316,42 +316,42 @@ namespace PetClinic.DAL.Migrations
                         new
                         {
                             Id = new Guid("0605974a-977c-4739-aa55-7e26e4eb2422"),
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Cat",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         },
                         new
                         {
                             Id = new Guid("c9a68d44-b5b8-4b96-9558-b4e52e750987"),
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Dog",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         },
                         new
                         {
                             Id = new Guid("13109317-ea78-4274-ad6e-e9a159f7f2f1"),
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Rabbit",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         },
                         new
                         {
                             Id = new Guid("a160449b-fb70-4991-9ddb-918b707829a8"),
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Parrot",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         },
                         new
                         {
                             Id = new Guid("f4dc2dab-9477-4ebe-8fb2-40306e739dee"),
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Hamster",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         });
                 });
 
@@ -511,26 +511,26 @@ namespace PetClinic.DAL.Migrations
                         new
                         {
                             Id = new Guid("2b513574-cabc-41ce-9fbc-e67255b84431"),
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Received",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         },
                         new
                         {
                             Id = new Guid("fb29bcb5-4493-4b03-b18e-11c50c650621"),
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Accepted",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         },
                         new
                         {
                             Id = new Guid("01b2b3b3-0f43-49c1-a138-dd39d76bb65a"),
-                            CreatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc),
+                            CreatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc),
                             IsDeleted = false,
                             Name = "Closed",
-                            UpdatedAt = new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc)
+                            UpdatedAt = new DateTime(2023, 6, 12, 2, 6, 3, 0, DateTimeKind.Utc)
                         });
                 });
 
