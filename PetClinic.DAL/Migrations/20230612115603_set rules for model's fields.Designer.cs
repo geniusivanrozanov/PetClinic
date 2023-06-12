fe967c4575ed447e61cf428a6c92819246386d8b
Veronika <veronikayatskova03@gmail.com>
2023-06-12 15:01:45 +0300

    Update database, change statuses configuration

--- PetClinic.DAL/Migrations/20230612115603_set rules for model's fields.Designer.cs
+++ PetClinic.DAL/Migrations/20230612115603_set rules for model's fields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PetClinic.DAL.DbContext;
@@ -11,9 +12,10 @@ using PetClinic.DAL.DbContext;
 namespace PetClinic.DAL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230612115603_set rules for model's fields")]
+    partial class setrulesformodelsfields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PetClinic.DAL/Migrations/20230612115603_set rules for model's fields.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetClinic.DAL.Migrations
+{
+    public partial class setrulesformodelsfields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: new Guid("328b1872-1141-47f5-8f67-62c50562ad39"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: new Guid("ddc19540-04df-4697-8237-3c74ff4e38cd"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: new Guid("de1e6cc5-3e62-4459-9496-8a5fc0b2593f"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("0605974a-977c-4739-aa55-7e26e4eb2422"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("13109317-ea78-4274-ad6e-e9a159f7f2f1"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("a160449b-fb70-4991-9ddb-918b707829a8"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("c9a68d44-b5b8-4b96-9558-b4e52e750987"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("f4dc2dab-9477-4ebe-8fb2-40306e739dee"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: new Guid("01b2b3b3-0f43-49c1-a138-dd39d76bb65a"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: new Guid("2b513574-cabc-41ce-9fbc-e67255b84431"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: new Guid("fb29bcb5-4493-4b03-b18e-11c50c650621"),
+                columns: new[] { "CreatedAt", "Name", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc), "Accepted", new DateTime(2023, 6, 11, 23, 56, 3, 0, DateTimeKind.Utc) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: new Guid("328b1872-1141-47f5-8f67-62c50562ad39"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: new Guid("ddc19540-04df-4697-8237-3c74ff4e38cd"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: new Guid("de1e6cc5-3e62-4459-9496-8a5fc0b2593f"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("0605974a-977c-4739-aa55-7e26e4eb2422"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("13109317-ea78-4274-ad6e-e9a159f7f2f1"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("a160449b-fb70-4991-9ddb-918b707829a8"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("c9a68d44-b5b8-4b96-9558-b4e52e750987"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "PetTypes",
+                keyColumn: "Id",
+                keyValue: new Guid("f4dc2dab-9477-4ebe-8fb2-40306e739dee"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: new Guid("01b2b3b3-0f43-49c1-a138-dd39d76bb65a"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: new Guid("2b513574-cabc-41ce-9fbc-e67255b84431"),
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+
+            migrationBuilder.UpdateData(
+                table: "Statuses",
+                keyColumn: "Id",
+                keyValue: new Guid("fb29bcb5-4493-4b03-b18e-11c50c650621"),
+                columns: new[] { "CreatedAt", "Name", "UpdatedAt" },
+                values: new object[] { new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc), "Our operator will call you soon", new DateTime(2023, 6, 11, 23, 51, 9, 0, DateTimeKind.Utc) });
+        }
+    }
+}
