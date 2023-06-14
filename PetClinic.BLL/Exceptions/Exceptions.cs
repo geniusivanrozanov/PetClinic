f1bf866f3e8c55087f8912cab9c8c21985a711b6
Martha Husar <martahusar757@gmail.com>
2023-06-14 17:58:58 +0300

    Made BaseRepository methods async

--- PetClinic.BLL/Exceptions/Exceptions.cs
+++ PetClinic.BLL/Exceptions/Exceptions.cs
@@ -6,4 +6,9 @@ public class Exceptions
     public static NotFoundException ServicesNotFound = new ("Services are not found");
     public static NotFoundException DepartmentNotFound = new ("Department with the given id does not exist");
     public static NotFoundException ServiceNotFound = new ("Service with the given id does not exist");
+    public static NotFoundException VetsNotFound = new("Vets are not found");
+    public static NotFoundException VetNotFound = new("Vet with the given id does not exist");
+    public static NotFoundException AppointmentNotFound = new("Appointment with the given id does not exist");
+    public static NotFoundException AppointmentsNotFound = new("Appointments are not found");
+
 }
