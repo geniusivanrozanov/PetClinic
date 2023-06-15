46e5928ce99348ed320bc78318f656e8999e492e
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-15 18:51:27 +0300

    add data access layer unit tests base

---
+++ PetClinic.DAL.Tests/Configuration.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PetClinic.DAL.Tests;
+
+public static class Configuration
+{
+    public const string ConnectionString = "Data Source=:memory:;";
+}
---
+++ PetClinic.DAL.Tests/DataAccessLayerUnitTestsBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using PetClinic.DAL.Repositories;
+
+namespace PetClinic.DAL.Tests;
+
+public abstract class DataAccessLayerUnitTestsBase : IDisposable
+{
+    private readonly AppDbContext _context;
+    protected readonly UnitOfWork RepositoryManager;
+
+    protected DataAccessLayerUnitTestsBase()
+    {
+        var sqliteConnection = new SqliteConnection(Configuration.ConnectionString);
+        sqliteConnection.Open();
+        
+        var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(sqliteConnection)
+            .Options; 
+        
+        _context = new AppDbContext(contextOptions);
+        _context.Database.EnsureDeleted();
+        _context.Database.EnsureCreated();
+        
+        RepositoryManager = new UnitOfWork(_context);
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+        RepositoryManager.Dispose();
+    }
+}
