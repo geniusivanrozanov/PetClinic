bdb0868bd86d135f24c27d15827b9fece5b6b9dc
Ivan Rozanov <navi.zor9@gmail.com>
2023-06-16 04:31:07 +0300

    add pet type repository unit tests as example

---
+++ PetClinic.DAL.Tests/RepositoriesTests/PetTypeRepositoryUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using PetClinic.DAL.Entities;
+using PetClinic.DAL.Interfaces.Repositories;
+using PetClinic.DAL.Repositories;
+using PetClinic.DAL.Tests.TestEntities;
+
+namespace PetClinic.DAL.Tests.RepositoriesTests;
+
+public class PetTypeRepositoryUnitTests : DataAccessLayerUnitTestsBase<PetTypeEntity>
+{
+    protected override IRepository<PetTypeEntity, Guid> Repository => RepositoryManager.PetTypeRepository;
+    
+    public PetTypeRepositoryUnitTests() : 
+        base(new PetTypeEntities())
+    {
+    }
+
+}
---
+++ PetClinic.DAL.Tests/TestEntities/PetTypeEntities.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq.Expressions;
+using PetClinic.DAL.Entities;
+
+namespace PetClinic.DAL.Tests.TestEntities;
+
+public class PetTypeEntities : EntitiesGenerator<PetTypeEntity>
+{
+    public override IEnumerable<PetTypeEntity> GetValidEntitiesList() => new[]
+    {
+        new PetTypeEntity()
+        {
+            Name = "Cat"
+        },
+        new PetTypeEntity()
+        {
+            Name = "Dog"
+        },
+        new PetTypeEntity()
+        {
+            Name = "Hamster"
+        }
+    };
+
+    public override IEnumerable<PetTypeEntity> GetEntitiesForSearch() => new[]
+    {
+        new PetTypeEntity()
+        {
+            Name = "Cat"
+        },
+        new PetTypeEntity()
+        {
+            Name = "Crab"
+        },
+        new PetTypeEntity()
+        {
+            Name = "Camel"
+        },
+        new PetTypeEntity()
+        {
+            Name = "Dog"
+        },
+        new PetTypeEntity()
+        {
+            Name = "Hamster"
+        }
+    };
+
+    public override Expression<Func<PetTypeEntity, bool>> GetFilterForSearch()
+    {
+        return (entity) => entity.Name.StartsWith("C");
+    }
+
+    public override void UpdateEntity(PetTypeEntity entity)
+    {
+        entity.Name = "Updated name";
+    }
+
+    public override PetTypeEntity CloneEntity(PetTypeEntity entity) => new PetTypeEntity()
+    {
+        Id = entity.Id,
+        Name = entity.Name,
+        CreatedAt = entity.CreatedAt,
+        UpdatedAt = entity.UpdatedAt,
+        IsDeleted = entity.IsDeleted
+    };
+
+    public override PetTypeEntity GetValidEntity() => new PetTypeEntity()
+    {
+        Name = "Cat"
+    };
+}
